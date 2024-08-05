f7d4410d4346bf0e402ed5b5bc45993f9b3532d5
MaheshReddy-05 <maheshreddy05bn@gmail.com>
2024-08-05 21:12:22 +0530

    Push Updated

--- Assignment-2/script.js
+++ Assignment-2/script.js
@@ -1,35 +1,47 @@
-const api = 'https://randomuser.me/api/'
+document.addEventListener('DOMContentLoaded', () => {
+    const api = 'https://randomuser.me/api/';
 
-let dataArray;
-fetch(api).then(
-    response =>{
-        if(!response.ok){
+    fetch(api).then(response => {
+        if (!response.ok) {
             throw new Error('Network response was not ok');
         }
         return response.json();
-    }).then(
-        data => {
-            console.log(data);
-            dataArray = data.results[0];
-            document.getElementById('fName').value = dataArray.name.first;
-            document.getElementById('lName').value = dataArray.name.last;
-            document.getElementById('eMail').value = dataArray.email;
-            document.getElementById('age').value = dataArray.dob.age;
-            document.getElementById('password').value = dataArray.login.password;
-            document.getElementById('cpassword').value = dataArray.login.password;
-            if(dataArray.gender == 'male'){
-                document.getElementById('male').checked = true;
-            }
-            else{
-                document.getElementById('female').checked = true;
-            }
-
-            document.getElementById('music').checked = true;
-            document.getElementById('upLoad').src = dataArray.picture.large;
+    }).then(data => {
+        console.log(data);
+        let dataArray = data.results[0];
+        document.getElementById('fName').value = dataArray.name.first;
+        document.getElementById('lName').value = dataArray.name.last;
+        document.getElementById('eMail').value = dataArray.email;
+        document.getElementById('age').value = dataArray.dob.age;
+        document.getElementById('password').value = dataArray.login.password;
+        document.getElementById('cpassword').value = dataArray.login.password;
+        if (dataArray.gender == 'male') {
+            document.getElementById('male').checked = true;
+        } else {
+            document.getElementById('female').checked = true;
         }
-    )
-    
-    
-    
+        document.getElementById('music').checked = true;
+    }).catch(error => {
+        console.error('Fetch error:', error);
+    });
 
-   
+    document.getElementById('accountForm').addEventListener('submit', function (event) {
+        event.preventDefault();
+        const formData = new FormData(this);
+        const data = Object.fromEntries(formData.entries());
+        console.log(data);
+
+        fetch('https://httpbin.org/post', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data),
+        }).then(res => res.json()).then(data => {
+            alert("Sucess");
+            console.log("Data uploaded successfully", data);
+        }).catch(error => {
+            console.log("Error while uploading data", error);
+        });
+    });
+});
